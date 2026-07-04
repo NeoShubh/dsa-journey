d4494d21d165efae8e5083a5562b6d2677d5f794
NeoShubh <shubhamrathore1024@gmail.com>
2026-07-04 22:39:46 +0530

    Two sum advanced

--- dsa/src/main/java/com/example/dsa/twoPointers/medium/TwoSumTwo167.java
+++ dsa/src/main/java/com/example/dsa/twoPointers/medium/TwoSumTwo167.java
@@ -1,17 +1,25 @@
 package com.example.dsa.twoPointers.medium;
 
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class TwoSumTwo167 {
-    public static void main(String [] args){
-        int [] nums = {2,7,11,15};
-        HashSet<Integer> st = new HashSet<>();
-        for(int num : nums){
-            if(st.contains(num)){
-                
-            }
-            else st.add(num);
-        }
+    public static void main(String[] args) {
 
+//        int[] nums = {2, 7, 11, 15};
+//        int[] nums = {2,3,4};
+        int[] nums = {-1,0};
+        int target = -1;
+        int [] ans = new int[2];
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            if(mp.containsKey(target-nums[i])){
+                ans[0]=mp.get(target-nums[i])+1;
+                ans[1]=i+1;
+            }else mp.put(nums[i],i);
+        }
+    for(int num:ans){
+        System.out.print(num+" ");
+    }
     }
 }
