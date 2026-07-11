4a8aeecf585c914926d47d74d33348f486ae421c
NeoShubh <shubhamrathore1024@gmail.com>
2026-07-11 12:38:37 +0530

    new solution for 424 leetcode

--- dsa/src/main/java/com/example/dsa/slidingWindow/LongestRepeatingCharacterReplacement424.java
+++ dsa/src/main/java/com/example/dsa/slidingWindow/LongestRepeatingCharacterReplacement424.java
@@ -1,27 +1,34 @@
 package com.example.dsa.slidingWindow;
 
-import java.lang.module.FindException;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
 
 public class LongestRepeatingCharacterReplacement424 {
 
     public static void main(String[] args) {
-        String s = "ABABBA";
+        String s = "ABAB";
         int k = 1;
-        int len = s.length();
-        int[] count = new int[26];
-        int start = 0, maxCount = 0, maxLength = 0;
-        for (int end = 0; end < len; end++) {
-            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
-            while (end - start + 1 - maxCount > k) {
-                count[s.charAt(start) - 'A']--;
-                start++;
-            }
-            maxLength = Math.max(maxLength, end - start + 1);
-        }
+        int left = 0;
 
+        int maxWindow = 0;
+        int maxFreq = 0;
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (int right = 0; right <= s.length() - 1; right++) {
+
+            hm.put(s.charAt(right), hm.getOrDefault(s.charAt(right), 0) + 1);
+
+            maxFreq = Math.max(maxFreq, hm.get(s.charAt(right)));
+
+            if ((right - left + 1) - maxFreq <= k) {
+
+                maxWindow = Math.max(maxWindow, right - left + 1);
+            } else {
+                hm.put(s.charAt(left), hm.get(s.charAt(left)) - 1);
+                left++;
+            }
+        }
+        System.out.println(hm);
+        System.out.println(maxFreq);
+        System.out.println(maxWindow);
     }
 
 
