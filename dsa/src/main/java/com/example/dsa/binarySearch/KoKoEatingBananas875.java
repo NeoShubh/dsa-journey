cf39489074b4a3469c1acf49b8047c7f405f718a
NeoShubh <shubhamrathore1024@gmail.com>
2026-07-25 20:58:28 +0530

    875

---
+++ dsa/src/main/java/com/example/dsa/binarySearch/KoKoEatingBananas875.java
@@ -0,0 +1,35 @@
+package com.example.dsa.binarySearch;
+
+import java.util.Arrays;
+
+public class KoKoEatingBananas875 {
+    public static void main(String[] args) {
+        int[] piles = {3, 6, 7, 11};
+        int h = 8;
+//1,2,3,4,5,6,7,8,9,10,11  1,2,3,4,5,6
+        int max = Arrays.stream(piles).max().getAsInt();
+        int l = 1;
+        int r = max;
+        int ans = Integer.MAX_VALUE;
+        while (r >= l) {
+            int k = l + (r - l) / 2;
+            int hours = 0;
+            System.out.println(k);
+            for (int i = 0; i < piles.length; i++) {
+
+                hours += Math.ceilDiv(piles[i], k);
+
+            }
+            System.out.println(hours);
+
+            if (hours <= h) {
+                System.out.println("ans before "+ans);
+                ans = Math.min(ans, k);
+                System.out.println("ans after "+ans);
+                r = k - 1;
+            } else l = k + 1;
+            System.out.println("______________");
+        }
+        System.out.println(ans);
+    }
+}
---
+++ dsa/src/main/java/com/example/dsa/binarySearch/MinInRotatedSortedArray153.java
@@ -0,0 +1,4 @@
+package com.example.dsa.binarySearch;
+
+public class MinInRotatedSortedArray153 {
+}
