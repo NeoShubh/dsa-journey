c909fec479ac9950590c8d239e3eb758d2f6ac7a
NeoShubh <shubhamrathore1024@gmail.com>
2026-08-03 21:35:46 +0530

    139 leetcode

---
+++ dsa/src/main/java/com/example/dsa/linkedList/CopyListWithRandomPointer138.java
@@ -0,0 +1,23 @@
+package com.example.dsa.linkedList;
+
+
+import java.util.HashMap;
+
+public class CopyListWithRandomPointer138 {
+    public static void main(String [] args){
+//        HashMap<ListNode,ListNode> hm = new HashMap<>();
+//        ListNode curr = ListNode;
+//
+//        while(curr!=null){
+//            hm.put(curr,new ListNode(curr.val));
+//            curr= curr.next;
+//        }
+//        curr = head;
+//        while(curr!=null){
+//            hm.get(curr).next=hm.get(curr.next);
+//            hm.get(curr).random=hm.get(curr.random);
+//            curr=curr.next;
+//        }
+//        return hm.get(head);
+    }
+}
